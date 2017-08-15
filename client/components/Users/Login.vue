db6d1397ef4acd86273b9b2e58521e8bca83edfd
Jiang-To <devtonyjiang@gmail.com>
2017-08-16 00:10:09 +0800

    compelete user menu

--- client/components/Users/Login.vue
+++ client/components/Users/Login.vue
@@ -1,41 +1,43 @@
 <template>
   <div>
-    <v-layout row wrap>
-      <v-flex xs6 offset-xs3>
-        <v-card>
-          <v-card-media class="white--text" height="100px">
-            <v-container blue-grey fill-height fluid>
-              <v-layout fill-height>
-                <v-flex xs12 align-center flexbox>
-                  <span class="headline">Login </span>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-card-media>
-          <v-card-title>
-            <v-container>
-              <v-layout column>
-                <!-- username -->
-                <v-flex xs10 offset-xs1>
-                  <v-text-field label="Username" id="username" prepend-icon="fa fa-user fa-2x" v-model="username"></v-text-field>
-                </v-flex>
-                <!-- password -->
-                <v-flex xs10 offset-xs1>
-                  <v-text-field label="Password" id="password" prepend-icon="fa fa-lock fa-2x" v-model="password" :append-icon="isPassowrdHidden ? 'visibility_off' : 'visibility'" :append-icon-cb="() => (isPassowrdHidden = !isPassowrdHidden)" :type="isPassowrdHidden ? 'password' : 'text'" counter></v-text-field>
-                </v-flex>
+    <form @submit.prevent="onLogin">
+      <v-layout row wrap>
+        <v-flex xs6 offset-xs3>
+          <v-card>
+            <v-card-media class="white--text" height="100px">
+              <v-container blue-grey fill-height fluid>
+                <v-layout fill-height>
+                  <v-flex xs12 align-center flexbox>
+                    <span class="headline">Login </span>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-card-media>
+            <v-card-title>
+              <v-container>
+                <v-layout column>
+                  <!-- username -->
+                  <v-flex xs10 offset-xs1>
+                    <v-text-field label="Username" id="username" prepend-icon="fa fa-user fa-2x" v-model="username"></v-text-field>
+                  </v-flex>
+                  <!-- password -->
+                  <v-flex xs10 offset-xs1>
+                    <v-text-field label="Password" id="password" prepend-icon="fa fa-lock fa-2x" v-model="password" :append-icon="isPassowrdHidden ? 'visibility_off' : 'visibility'" :append-icon-cb="() => (isPassowrdHidden = !isPassowrdHidden)" :type="isPassowrdHidden ? 'password' : 'text'" counter></v-text-field>
+                  </v-flex>
   
-                <v-flex xs4 offset-xs4>
-                  <v-btn block class="blue-grey white--text" :loading="isLoggingIn" @click.native="login">
-                    Login
-                    <span slot="loader">Logging In...</span>
-                  </v-btn>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-card-title>
-        </v-card>
-      </v-flex>
-    </v-layout>
+                  <v-flex xs4 offset-xs4>
+                    <v-btn block class="blue-grey white--text" :loading="isLoggingIn" type="submit">
+                      Login
+                      <span slot="loader">Logging In...</span>
+                    </v-btn>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-card-title>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </form>
   </div>
 </template>
 
@@ -52,14 +54,17 @@ export default {
     }
   },
   methods: {
-    login () {
+    onLogin () {
       this.isLoggingIn = true;
       this.$store.dispatch('login', { username: this.username, password: this.password })
         .then(() => {
           // redirect to the previous router or home page
           let redirectPath = this.$router.currentRoute.query.from || '/'
           this.$router.push(redirectPath)
-        });
+        })
+        .catch(error => {
+          console.log(error);
+        })
     }
   },
   store: AuthStore
--- client/components/common/AppUser.vue
+++ client/components/common/AppUser.vue
@@ -1,23 +1,19 @@
 <template>
   <v-toolbar-items class="hidden-sm-and-down">
     <v-btn flat v-if="!isAuthenticated" to="/login">Login</v-btn>
-    <v-btn flat v-if="isAuthenticated" to="/">{{ user.username }} </v-btn>
+    <!-- <v-btn flat v-if="isAuthenticated" to="/">{{ user.username }} </v-btn> -->
   
-    <v-menu id="user-menu">
-      <v-btn slot="activator">
-        <span>All</span>
+    <v-menu id="user-menu" class="indigo" v-if="isAuthenticated">
+      <v-btn flat slot="activator" class="indigo">
+        <span>{{ user.username }} </span>
         <v-icon dark>arrow_drop_down</v-icon>
       </v-btn>
       <v-list id="user-menu-options">
-        <!-- <v-list-tile>
-            <v-list-tile-title>Logout 1</v-list-tile-title>
-          </v-list-tile> -->
-        <v-list-tile v-for="item in userMenus" :key="item.title">
+        <v-list-tile v-for="item in userMenus" :key="item.title" :to="item.path">
           <v-list-tile-title>{{ item.title }}</v-list-tile-title>
         </v-list-tile>
       </v-list>
     </v-menu>
-  
   </v-toolbar-items>
 </template>
 
@@ -32,7 +28,7 @@ export default {
         path: '/profile'
       }, {
         title: 'Logout',
-        path: '/logut'
+        path: '/logout'
       }]
     }
   },
@@ -53,7 +49,8 @@ export default {
   height: 64px;
   margin-top: 0px;
 }
+
 #user-menu-options {
-  margin-top:64px;
+  margin-top: 64px;
 }
 </style>
